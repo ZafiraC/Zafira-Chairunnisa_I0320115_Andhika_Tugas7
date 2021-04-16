526138fbac95abf5d8aa4adcf1e5fe0915e6e9f4
ZafiraC <80144785+ZafiraC@users.noreply.github.com>
2021-04-16 21:30:32 +0700

    Mengumpulkan Tugas 7
    Mengumpulkan Tugas 7

---
+++ I0320115_Soal 1.py
@@ -0,0 +1,21 @@
+# SOAL NO 1
+# Fungsi Metode Center
+str = "Selamat Menunaikan Ibadah Puasa"
+s = str.center(50,'^')
+print(s)
+print(" ")
+# Fungsi Metode Replace
+str = "Selamat Menunaikan Ibadah Puasa"
+print("str: "+str)
+print("str replace Menunaikan= ", str.replace("Menunaikan","Menjalankan"))
+print(" ")
+# Fungsi Metode Capitalize dan Metode Endswith
+str = "puasa hari pertama seru dan menyenangkan"
+c = str.capitalize()
+print(c)
+print("Apakah puasa hari pertama menyenangkan?", str.endswith('menyenangkan'))
+print(" ")
+# Fungsi Metode Upper
+str = "semoga puasanya lancar sampai akhir dan semua amal ibadah diterima oleh Allah SWT"
+u = str.upper()
+print(u)
---
+++ I0320115_Soal 2.py
@@ -0,0 +1,29 @@
+# SOAL NO 2
+# Fungsi min() dan max()
+print("Ada empat siswa yang mengikuti lomba lari tingkat sekolah, yaitu Aksa, Beni, Ciko, dan Deru. Aksa mecetak waktu 4.20 menit, Beni dengan waktu 5.02 menit, Ciko 4.11 menit, dan Deru 4.50 menit. Dari waktu yang diperoleh keempat pelari, tentukan pemenangnya dengan menggunakan fungsi numerik min() dan max().")
+import math
+Aksa = 4.20
+Beni = 5.02
+Ciko = 4.11
+Deru = 4.50
+print("Pemenang lomba lari tingkat sekolah adalah siswa dengan perolehan waktu lari", min(Aksa,Beni,Ciko,Deru), "menit, yaitu Ciko")
+print(" ")
+
+# Fungsi abs()
+a = -112
+b = -90
+c = 25
+print("Tentukan harga mutlak bilangan dibawah ini!")
+print("a= ", a)
+print("b= ", b)
+print("c= ", c)
+print("Harga mutlak a adalah ", abs(a))
+print("Harga mutlak b adalah ", abs(b))
+print("Harga mutlak c adalah ", abs(c))
+print(" ")
+
+# Fungsi pow()
+print("Tentukan hasil dari 12 pangkat 3 dengan menggunakan fungsi pow")
+a = 3
+b = 12
+print("Hasil dari", b, "pangkat", a, "adalah", pow(b,a))
